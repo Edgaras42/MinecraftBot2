6e1211244267f6f47821021f6f4cc79496bbf9c6
Edgaras42 <edgaras.norvilas2002@gmail.com>
2025-03-13 12:21:19 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,43 +1,22 @@
 const mineflayer = require('mineflayer');
 
-let bot;
+// Sukuriame botą ir prisijungiame prie serverio
+const bot = mineflayer.createBot({
+  host: 'Chillkmrkarocia.aternos.me',  // Serverio IP
+  port: 23210,                        // Serverio prievartas
+  username: 'SERVAS24/7',              // Bot vardas
+  version: '1.21.4',                   // Minecraft versija
+  auth: 'offline'                      // Offline režimas
+});
 
-function createBot() {
-  bot = mineflayer.createBot({
-    host: 'Chillkmrkarocia.aternos.me',  // Serverio IP
-    port: 23210,                        // Serverio prievartas
-    username: 'SERVAS24/7',              // Bot vardas
-    version: '1.21.4',                   // Minecraft versija
-    auth: 'offline'                      // Offline režimas
-  });
+bot.on('spawn', () => {
+  console.log('Botas prisijungė prie serverio!');
+});
 
-  bot.on('spawn', () => {
-    console.log('Botas prisijungė prie serverio!');
-    sendMessagePeriodically(); // Pradeda siųsti žinutę kas 30 minučių
-  });
+bot.on('end', (reason) => {
+  console.log(`Botas atsijungė dėl priežasties: ${reason}`);
+});
 
-  bot.on('end', (reason) => {
-    console.log(`Botas atsijungė dėl priežasties: ${reason}`);
-    // Bandykite prisijungti vėl po 3 minutės
-    setTimeout(createBot, 180000); // 3 minutės
-  });
-
-  bot.on('error', (err) => {
-    console.log('Klaida:', err);
-    // Bandykite prisijungti vėl po 3 minutės
-    setTimeout(createBot, 180000); // 3 minutės
-  });
-}
-
-// Periodiškai siunčia žinutę
-function sendMessagePeriodically() {
-  setInterval(() => {
-    if (bot && bot.chat) {
-      bot.chat('Kon jus balvoneliai');
-      console.log('Botas išsiuntė žinutę: "Kon jus balvoneliai"');
-    }
-  }, 1800000); // Kas 30 minučių (30 minutės * 60 sekundžių * 1000 milisekundės)
-}
-
-// Sukuriame pirmą botą
-createBot();
+bot.on('error', (err) => {
+  console.log('Klaida:', err);
+});
